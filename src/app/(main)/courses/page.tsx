700c53d758118246f461d8ec7f7cbe9f2d39b506
lanoow <me@lanoow.dev>
2025-05-29 17:00:36 +0300

    fixes

--- src/app/(main)/courses/page.tsx
+++ src/app/(main)/courses/page.tsx
@@ -15,9 +15,14 @@ export default async function CoursesPage() {
 
 	return (
 		<div className="h-full max-w-[912px] px-3 mx-auto">
-			<h1 className="text-2xl font-bold text-neutral-700">
-				Language Courses
-			</h1>
+			<div>
+				<h1 className="text-2xl font-bold text-neutral-700">
+					Language Courses
+				</h1>
+				<p className="text-neutral-500">
+					Explore our language courses and track your progress. Select a course to start learning or continue where you left off.
+				</p>
+			</div>
 
 			<List
 				courses={courses}
